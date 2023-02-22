07416856f378b81266cd0c360854af3437bfef11
Watati8 <yowatatingaruiya@gmail.com>
2023-02-21 23:40:05 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,21 @@
 #include "main.h"
 
 /**
-  main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Entry
+ * description: prints out _putchar
+ * Return: 0;
  */
-int main(void)
+int main void 
 {
-	char ch = '1';
-  
-    for (ch = '1'; ch <= '9'; ch++)
-        putchar(ch);
-  
-    return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');	
+	_putchar('\n');
+
+	return (0);
 }
