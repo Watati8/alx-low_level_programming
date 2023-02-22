540e36eb566c539be8cada3ded77b6f18d9987e9
Watati8 <yowatatingaruiya@gmail.com>
2023-02-21 17:21:46 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,9 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{  
-    // Get the character to be written
-    char ch = '1';
+{
+	char ch = '1';
   
-    // Write the Character to stdout
     for (ch = '1'; ch <= '9'; ch++)
         putchar(ch);
   
