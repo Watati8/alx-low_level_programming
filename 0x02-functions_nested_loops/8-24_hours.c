27c38a673b06dadbb0b21adec3c0cb96d9005c24
Watati8 <yowatatingaruiya@gmail.com>
2023-02-22 03:36:55 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,10 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
- *
+ * jack_bauer - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
  */
-void jack_bauer(void);
+void jack_bauer(void)
 {
 	int hour, minute;
 
