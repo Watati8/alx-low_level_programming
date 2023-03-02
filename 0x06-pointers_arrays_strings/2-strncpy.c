833792d136bf88c7d405410ba8be5c733831e592
Watati8 <yowatatingaruiya@gmail.com>
2023-03-02 02:32:19 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * _strncpy - copy a string
+ *
+ * @dest: arg a
+ *
+ * @src: arg b
+ *
+ * @n: arg n
+ *
+ * Return: void.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+
+	dest[i] = '\0';
+
+
+	return (dest);
+}
